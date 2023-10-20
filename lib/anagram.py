f51a6c15af96f7f6a644c8c62d9c3c51fd2ada19
Abigail <abby.a.otieno@gmail.com>
2023-10-20 12:23:42 +0300

    Completed Lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,20 @@
 # your code goes here!
+class Anagram:
+    def __init__(self,word):
+        self.word = word
+    
+    def match(self,list):
+            
+        matching_words = []
+        word_list = [letter for letter in self.word]
+        for word in list:
+            anagram = [char for char in word]
+            if sorted(word_list) == sorted(anagram):
+                matching_words.append(word)
+
+        if len(matching_words) == 0:
+            return []
+        else:
+            return matching_words
+            
+
